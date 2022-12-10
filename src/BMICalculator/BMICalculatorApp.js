7baa01c87bd6be4da88d0287d57df60fdca1b664
Chandan <Chandan17ee@gmail.com>
2022-12-10 22:08:43 +0530

    feat: used promise constructor

--- src/BMICalculator/BMICalculatorApp.js
+++ src/BMICalculator/BMICalculatorApp.js
@@ -13,22 +13,65 @@ const BMICalculatorAPPs = () => {
   // Here we used Toast to show the messages.
   const HandleOnClick = (e) => {
     e.preventDefault();
-    if (weight > 0 && height > 0) {
-      let bmi = (weight / (height * height)) * 703;
-      setBmi(bmi.toFixed(1));
-      if (bmi < 25) {
-        setMessage("You are underweight");
-      } else if (bmi >= 25 && bmi < 30) {
-        setMessage("You are a healthy weight");
-      } else {
-        setMessage("You are overweight");
-      }
 
-      toast.info("Please Reload the page...");
-    } else {
-      setMessage("Please type valid number");
-      toast.warning("Invalid Number...", { autoClose: 1500 });
-    }
+    //                             Here we used Promise.
+
+    // The promise constructor accepts a callback function that typically performs an asynchronous
+    //  operation. This function is often referred to as an executor.
+
+    // In turn, the executor accepts two callback functions with the name resolve and reject.
+
+    //                            Note :
+    //   the callback functions passed into the executor are resolve and reject by convention only.
+
+    // If the asynchronous operation completes successfully, the executor will call the resolve() function
+    // to change the state of the promise from pending to fulfilled with a value.
+
+    // In case of an error, the executor will call the reject() function to change
+    //  the state of the promise from pending to rejected with the error reason.
+
+    return new Promise((resolve, reject) => {
+      if (weight > 0 && height > 0) {
+        let bmi = (weight / (height * height)) * 703;
+        resolve(setBmi(bmi.toFixed(1)));
+        if (bmi < 25) {
+          resolve(setMessage("You are underweight"));
+        } else if (bmi >= 25 && bmi < 30) {
+          resolve(setMessage("You are a healthy weight"));
+        } else {
+          resolve(setMessage("You are overweight"));
+        }
+        toast.info("Please Reload the page...");
+      } else {
+        reject(
+          setMessage("Please type valid number"),
+          toast.warning(
+            "Invalid number,Please reload the page and type valid number",
+            { autoClose: 1500 }
+          )
+        );
+      }
+    });
+
+    //           Or We can use Like this as well
+
+    // if (weight > 0 && height > 0) {
+    //   let bmi = (weight / (height * height)) * 703;
+    //   setBmi(bmi.toFixed(1));
+    //   if (bmi < 25) {
+    //     setMessage("You are underweight");
+    //   } else if (bmi >= 25 && bmi < 30) {
+    //     setMessage("You are a healthy weight");
+    //   } else {
+    //     setMessage("You are overweight");
+    //   }
+
+    //   toast.info("Please Reload the page...");
+    // } else {
+    //   setMessage("Please type valid number");
+    //   toast.warning("Invalid Number...", { autoClose: 1500 });
+    // }
+    
   };
 
   const ReloadFunction = (e) => {
